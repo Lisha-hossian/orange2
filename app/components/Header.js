75fdc7167125d31b596e2224697171adee882a87
Lisha-hossian <nasema.akter@students.beaminstitute.org>
2025-02-27 09:47:11 +0100

    Edited one

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className=''>
       <div className='bg-grey-50 text-black border-1 border-blue-500 border-opacity-50 flex items-center'>
-        <nav className=' max-w-screen-2xl container flex mx-auto items-center justify-between py-6 px-4 flex-wrap'>
+        <nav className='max-w-screen-2xl container flex mx-auto items-center justify-between py-6 px-4 flex-wrap'>
           <div className='hidden sm:block'>
             <ul className='flex font-regular font-semibold text-sm space-x-10 mx-4 md:flex-row items-center'>
               <li className='hover:text-yellow-500 hover:scale-105 transition-all  hover:decoration-red-500'>
@@ -70,7 +70,7 @@ export default function Header() {
               <li className='hover:text-yellow-500 hover:scale-105 transition-all '>
                 <Link href='/wishlist'>Wishlist</Link>
               </li>
-              <li className='flex'>
+              <li className='flex '>
                 <Link href='/'>
                   {' '}
                   <svg
@@ -87,7 +87,7 @@ export default function Header() {
                       d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
                     />
                   </svg>
-                  <sub className='absolute top-12 right-28 text-sm bg-orange-700 w-4 h-4 text-center rounded-full'>
+                  <sub className='lg:absolute top-12 right-28 text-sm bg-orange-700 w-5 h-5 text-center rounded-full'>
                     0
                   </sub>
                 </Link>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import heroImage from './image/img2.jpg';
-import bgImage from './image/img3.png';
+import bgImage from './image/imgM11.png';
 import headImage from './image/img.png';
 import secondImage from './image/img4.png';
 import lastImage from './image/last-img.png';
@@ -12,8 +12,8 @@ import sixthOImage from './image/img8.png';
 import seventhTImage from './image/img9.png';
 import eightTImage from './image/img10.png';
 import ninthFImage from './image/img11.png';
-import tenthMImage from './image/imgM16.png';
-import eleventhMImage from './image/imgM17.png';
+import tenthMImage from './image/imgM12.png';
+import eleventhMImage from './image/imgM13.png';
 
 export default function Home() {
   return (
@@ -104,8 +104,8 @@ export default function Home() {
             </ul>
           </div>
         </section>
-        <div className='flex items-center justify-center  my-4 py-4 w-screen h-10 bg-slate-50 text-4xl font-semibold'>
-          <ul className='flex gap-20'>
+        <div className='flex items-center justify-center  my-4 py-4 w-screen h-10 bg-slate-50 lg:text-4xl font-semibold'>
+          <ul className='flex gap-20 flex-wrap'>
             <li>Summer Collection 2025</li>
             <li className=''>UP TO 35% OFF</li>
           </ul>
@@ -119,9 +119,9 @@ export default function Home() {
                   src={thirdImage}
                   alt='img5'
                 />
-                <div className='item-details flex justify-between ml-8 mr-2 mt-2 mb-4'>
-                  <span className='fashion-item-name'>Green-Outfit</span>
-                  <span className='fashion-item-price'>€1.999,00</span>
+                <div className='flex justify-between ml-8 mr-2 mt-2 mb-4'>
+                  <span className=''>Green-Outfit</span>
+                  <span className=''>€1.999,00</span>
                 </div>
               </li>
               <li className='w-full lg:w-4/12'>
@@ -131,8 +131,8 @@ export default function Home() {
                   alt='img6'
                 />
                 <div className='flex justify-between mt-2 mb-4'>
-                  <span className='fashion-item-name'>Casual-Outfit</span>
-                  <span className='fashion-item-price'>€509,00</span>
+                  <span className=''>Casual-Outfit</span>
+                  <span className=''>€509,00</span>
                 </div>
               </li>
               <li className='w-full lg:w-4/12'>
@@ -157,13 +157,13 @@ export default function Home() {
               <li className=''>
                 {' '}
                 <Image
-                  className='w-[370] h-[518]'
+                  className='w-[370] h-[518] object-cover'
                   src={sixthOImage}
                   alt='img8'
                 />
                 <div className='text-center'>
                   <Link href={'/women'}>
-                    <span className='font-bold text-xl hover:underline'>
+                    <span className='font-bold lg:text-xl hover:underline hover:text-yellow-500 hover:scale-105 transition-all'>
                       PARTY-DRESS
                     </span>
                   </Link>
@@ -173,14 +173,14 @@ export default function Home() {
               <li>
                 {' '}
                 <Image
-                  className='w-[370] h-[518]'
+                  className='w-[370] h-[518] object-cover'
                   src={seventhTImage}
                   alt='img9'
                 />
                 <div className='text-center '>
                   <Link href={'/women'}>
                     {' '}
-                    <span className='font-bold text-xl hover:underline'>
+                    <span className='font-bold lg:text-xl hover:underline hover:text-yellow-500 hover:scale-105 transition-all'>
                       PANT-BLAZER
                     </span>
                   </Link>
@@ -189,14 +189,14 @@ export default function Home() {
               <li>
                 {' '}
                 <Image
-                  className='w-[370] h-[518]'
+                  className='w-[370] h-[518] object-cover'
                   src={eightTImage}
                   alt='img10'
                 />
                 <div className='text-center '>
                   <Link href={'/women'}>
                     {' '}
-                    <span className='font-bold text-xl hover:underline'>
+                    <span className='font-bold lg:text-xl hover:underline hover:text-yellow-500 hover:scale-105 transition-all'>
                       CASUAL-DRESSES
                     </span>
                   </Link>
@@ -204,14 +204,14 @@ export default function Home() {
               </li>
               <li>
                 <Image
-                  className='w-[370] h-[518]'
+                  className='w-[370] h-[518] object-cover'
                   src={ninthFImage}
                   alt='img11'
                 />
                 <div className='text-center '>
                   <Link href={'/women'}>
                     {' '}
-                    <span className='font-bold text-xl hover:underline'>
+                    <span className='font-bold lg:text-xl hover:underline hover:text-yellow-500 hover:scale-105 transition-all'>
                       COATS
                     </span>
                   </Link>
@@ -221,74 +221,130 @@ export default function Home() {
           </div>
         </section>
         <section>
-          <div className='flex font-semibold text-xl justify-evenly my-4 py-4'>
-            <h2>FOR NEW CASUAL LOOK COLLECTION</h2>
+          <div className='flex font-semibold lg:text-xl justify-evenly bg-slate-100 my-4 py-4'>
+            <Link
+              className='hover:text-yellow-500 hover:scale-105 transition-all'
+              href={'/men'}
+            >
+              FOR NEW CASUAL LOOK COLLECTION
+            </Link>
 
-            <Link className='flex underline items-center' href={'/men'}>
+            <Link
+              className='flex underline items-center hover:text-yellow-500 hover:scale-105 transition-all'
+              href={'/men'}
+            >
               CLICK HERE
             </Link>
           </div>
         </section>
         <section className=''>
-          <div className='relative'>
+          {/* <div className='relative'>
             <div className='flex  justify-evenly my-6'>
-              <Image className='w-[500] h-[750]' src={bgImage} alt='' />
-              <div className='flex absolute top-1/2 left-52 underline'>
-                <Link className='font-semibold' href={'/men'}>
+              <Image className='lg:w-[500] h-[750]' src={bgImage} alt='' />
+              <div className='flex lg:absolute top-1/2 left-52 underline '>
+                <Link className='font-semibold lg: ' href={'/men'}>
                   OFFICE-WEAR
                 </Link>
               </div>
               <div className='flex justify-evenly  bottom-40 left-52 '>
                 <Image className='w-[500] h-[750]' src={tenthMImage} alt='' />
-                <div className='flex absolute top-1/2 right-40 underline'>
+                <div className='flex lg:absolute top-1/2 right-40 underline'>
                   <Link className='font-semibold' href={'/men'}>
                     CASUAL-WEAR
                   </Link>
                 </div>
               </div>
-              <div className='flex bottom-1/4 left-52'>
+              <div className='flex bottom-1/4 left-52 lg:'>
                 <Image
                   className='w-[500] h-[750]'
                   src={eleventhMImage}
                   alt=''
                 />
-                <div className='flex absolute bottom-1/2 left-1/2 underline'>
+                <div className='flex lg:absolute bottom-1/2 left-1/2 underline'>
                   <Link className='font-semibold' href={'/men'}>
                     SUMMER-WEAR
                   </Link>
                 </div>
               </div>
-            </div>
-            <div className='flex font-semibold text-2xl justify-end my-4 py-4'>
-              <ul className='flex items-center'>
-                <li>MEN NEW COLLECTION </li>
-                <li>
-                  <Link className='flex items-center' href={'/men'}>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='54'
-                      height='54'
-                      viewBox='0 0 54 54'
-                      fill='none'
-                      className='flex items-center m-2'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        clipRule='evenodd'
-                        d='M5.40002 27C5.40002 25.8816 6.30665 24.975 7.42502 24.975L41.4183 24.975L35.7471 19.7089C34.9276 18.9479 34.8801 17.6666 35.6411 16.847C36.4021 16.0275 37.6834 15.9801 38.5029 16.7411L47.9529 25.5161C48.3656 25.8992 48.6 26.4369 48.6 27C48.6 27.563 48.3656 28.1007 47.9529 28.4839L38.5029 37.2589C37.6834 38.0199 36.4021 37.9724 35.6411 37.1529C34.8801 36.3333 34.9276 35.0521 35.7471 34.2911L41.4183 29.025L7.42502 29.025C6.30665 29.025 5.40002 28.1183 5.40002 27Z'
-                        fill='black'
-                      />
-                    </svg>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            </div> */}
+          <div className='flex justify-evenly relative '>
+            <ul className='flex gap-2 '>
+              <li className=''>
+                <Image
+                  className='lg:w-[500] h-[750] object-cover'
+                  src={bgImage}
+                  alt=''
+                />
+              </li>
+              <div className='flex lg:absolute top-1/2 left-80  text-white font-bold underline  decoration-orange-400 hover:text-yellow-500 hover:scale-105 transition-all'>
+                <Link className='' href={'/men'}>
+                  SWEAT
+                </Link>
+              </div>
+              <li className=''>
+                <Image
+                  className='lg:w-[500] h-[750] object-cover '
+                  src={tenthMImage}
+                  alt=''
+                />
+              </li>
+              <div className='flex lg:absolute top-1/2 left-1/2 text-white font-bold underline  decoration-orange-400  hover:text-yellow-500 hover:scale-105 transition-all'>
+                <Link className='' href={'/men'}>
+                  SUITS
+                </Link>
+              </div>
+              <li className=''>
+                <Image
+                  className='lg:w-[500] h-[750] object-cover'
+                  src={eleventhMImage}
+                  alt=''
+                />
+              </li>
+              <div className='flex lg:absolute bottom-1/2 right-80  text-white font-bold underline decoration-orange-400  hover:text-yellow-500 hover:scale-105 transition-all'>
+                <Link className='' href={'/men'}>
+                  CASUAL
+                </Link>
+              </div>
+            </ul>
+          </div>
+          <div className='flex font-semibold text-2xl justify-end my-4 py-4'>
+            <ul className='flex items-center'>
+              <li>
+                {' '}
+                <Link
+                  className='hover:text-yellow-500 hover:scale-105 transition-all'
+                  href={'/men'}
+                >
+                  {' '}
+                  MEN NEW COLLECTION{' '}
+                </Link>
+              </li>
+              <li>
+                <Link className='flex items-center' href={'/men'}>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='54'
+                    height='54'
+                    viewBox='0 0 54 54'
+                    fill='none'
+                    className='flex items-center m-2'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d='M5.40002 27C5.40002 25.8816 6.30665 24.975 7.42502 24.975L41.4183 24.975L35.7471 19.7089C34.9276 18.9479 34.8801 17.6666 35.6411 16.847C36.4021 16.0275 37.6834 15.9801 38.5029 16.7411L47.9529 25.5161C48.3656 25.8992 48.6 26.4369 48.6 27C48.6 27.563 48.3656 28.1007 47.9529 28.4839L38.5029 37.2589C37.6834 38.0199 36.4021 37.9724 35.6411 37.1529C34.8801 36.3333 34.9276 35.0521 35.7471 34.2911L41.4183 29.025L7.42502 29.025C6.30665 29.025 5.40002 28.1183 5.40002 27Z'
+                      fill='black'
+                    />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
           </div>
         </section>
         <div className=' flex  border border-red-600 h-12  bg-red-500 gap-2 justify-center'>
           <ul className='flex gap-4 items-center'>
             <li>
-              <span className='font-semibold my-2'>SALE UP TO-70% </span>
+              <span className='font-semibold my-2'>SALE UP TO-70%</span>
             </li>
             <li>
               <span className='text-xs my-2'>Last reduction</span>
@@ -301,23 +357,23 @@ export default function Home() {
 
         <section className='relative'>
           <div className='flex '>
-            <Image className='rounded-lg' src={lastImage} alt='' />
+            <Image className='h-[400px] object-fill' src={lastImage} alt='' />
 
             <div className='hidden lg:flex container my-6 absolute right-[20] items-center border border-black-700 w-1/2 lg:w-50 h-72 bg-white font-medium'>
               <div className=' flex '>
                 <h2
-                  className='absolute top-10
+                  className='absolute top-10 left-4
               font-bold text-3xl m-6'
                 >
                   Orange newsletter
                 </h2>
                 <p className='flex flex-wrap text-center leading-8'>
-                  <span className='absolute bottom-36  mx-6 mb-2'>
+                  <span className='absolute bottom-36  mx-4 mb-2'>
                     With the Orange newsletter you are always up to date about
-                    new
+                    new products,
                   </span>
-                  <span className=' absolute bottom-32 left-8 mx-6'>
-                    products, exclusive specials, lifestyle and fashion trends.
+                  <span className=' absolute bottom-32 left-6 mx-2'>
+                    exclusive specials, lifestyle and fashion trends.
                   </span>
                 </p>
                 <div className=' absolute bottom-6 mx-6 my-4 '>
